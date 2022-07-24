766d9508594bcd6c15d0bf601fc03c7d5f3c1379
Gracious Nguwo <graciousnguwo2020@gmail.com>
2022-07-24 23:01:26 +0200

    routes and coin search component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import {ThemeProvider} from './context/ThemeContext'
+import Home from './routes/Home';
+import Signin from './routes/Signin';
+import Signup from './routes/Signup';
+import Account from './routes/Account';
 
 
 function App() {
   return (
     <ThemeProvider>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/signin" element={< Signin/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/account" element={<Account/>} />
+      </Routes>
     </ThemeProvider>
   );
 }
---
+++ src/routes/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CoinSearch from '../components/CoinSearch'
+
+const Home = () => {
+  return (
+    <div>
+        <CoinSearch/>
+    </div>
+  )
+}
+
+export default Home
