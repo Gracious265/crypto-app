fd7c057e40f4e3beeac049d7389e833a2f9b6254
Gracious Nguwo <graciousnguwo2020@gmail.com>
2022-07-26 15:10:47 +0200

    dependancies modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import {ThemeProvider} from './context/ThemeContext'
-import Home from './routes/Home';
-import Signin from './routes/Signin';
-import Signup from './routes/Signup';
-import Account from './routes/Account';
 
+import React, { useEffect } from "react";
+import {  Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { ThemeProvider } from "./context/ThemeContext";
+import Home from "./routes/Home";
+import Signin from "./routes/Signin";
+import Signup from "./routes/Signup";
+import Account from "./routes/Account";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true";
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data);
+      console.log(response.data);
+    });
+  }, [url]);
+
   return (
     <ThemeProvider>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/signin" element={< Signin/>} />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/account" element={<Account/>} />
-      </Routes>
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/account" element={<Account />} />
+      </Routes> */}
     </ThemeProvider>
   );
 }
